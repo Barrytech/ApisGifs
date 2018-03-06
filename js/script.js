96167b90cd49d6577bb5fbcca048a551c4abfe66
Barry <barry@citypolyhs.org>
2018-03-06 23:27:12 +0000

    started

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
+/* global $ */
 $(document).ready(function(){
   
+  //input id="srch-term"
+  $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+          
+          $('.gallery').html("<img src= "+ response.data[0].images.original.url +">");
+          
+      },
+    }); 
+
   
   
   
